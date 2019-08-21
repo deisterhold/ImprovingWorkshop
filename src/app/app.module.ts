dd484af3a791dc84447635c97d31777ad73b5af9
Daniel Eisterhold <deisterhold@gmail.com>
2019-08-21 14:26:51 -0500

    Add box and grid components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BoxComponent } from './box/box.component';
+import { GridComponent } from './grid/grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoxComponent,
+    GridComponent
   ],
   imports: [
     BrowserModule
